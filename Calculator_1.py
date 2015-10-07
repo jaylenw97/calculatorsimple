5c693d7648dd8a83eb3660686645361ca5a22059
Jaylen White <wht.jln@gmail.com>
2015-10-06 20:17:11 -0400

    Added comments and multiply

--- Calculator_1.py
+++ Calculator_1.py
@@ -2,13 +2,19 @@ __author__ = 'jasonmerlo'
 
 
 def main():
+    #Add
     num_a = int(input("Enter a number to add "))
     num_b = int(input("Enter another number to add: "))
     print("{}".format(num_a + num_b))
+
+    #Subtract
     num_a = int(input("Enter a number to subtract from: "))	
     num_b = int(input("Enter a second number to subtract from: "))
     print("{}".format(num_a - num_b))
-
-
+   
+    #Multiply
+    num_a = int(input("Enter a number to multiply from: "))
+    num_b = int(input("Enter a second number to multiply from: "))
+    print("{}".format(num_a * num_b))
 
 main()
